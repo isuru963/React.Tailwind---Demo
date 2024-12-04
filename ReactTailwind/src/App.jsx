a34194aee1467899bb8da22ed4bf0b08494775b6
Isuru S. Aththanayake <118103842+isuru963@users.noreply.github.com>
2024-12-04 09:40:41 +0530

    setup3

--- ReactTailwind/src/App.jsx
+++ ReactTailwind/src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header"
 function App() {
   
 
   return (
     <>
-      <h1>Hello Tailwind</h1>
+    <Header/>
+      <h1 className="bg-slate-600 text-yellow-400">Hello Tailwind</h1>
     </>
   )
 }
---
+++ ReactTailwind/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="text-orange-500 font-bold">Header</div>
+  )
+}
+
+export default Header
