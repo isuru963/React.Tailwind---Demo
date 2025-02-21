350c23148e5e3add5887ec7e95f9c49a58a2d257
Isuru S. Aththanayake <isuruaththanayake00@gmail.com>
2025-02-21 19:58:00 +0530

    Update temp_code.js

--- temp_code.js
+++ temp_code.js
@@ -1,3 +1,6 @@
 import React from "react";
 const App = () => {
   return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">Hello, React!</h1>
+    </div>
