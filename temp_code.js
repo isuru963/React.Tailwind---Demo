3e7a442cb62fd6ffcde4e97b8951150b8267faa3
Isuru S. Aththanayake <isuruaththanayake00@gmail.com>
2025-02-23 15:57:38 +0530

    Update temp_code.js

--- temp_code.js
+++ temp_code.js
@@ -10,6 +10,10 @@ const App = () => {
     </div>
   <div className="searc bar position = "center" bg-gray-100:>
     </div>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="p-6 bg-white shadow-lg rounded-lg text-center">
+        <h1 className="text-4xl font-bold text-blue-600">Hello, React!</h1>
+        <p className="text-gray-700 mt-2">Welcome to your simple React app.</p>
   );
 };
 
