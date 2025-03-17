b9b7524dc7ab1bdef828ab4f68ee2c6abe6a955c
Isuru S. Aththanayake <isuruaththanayake00@gmail.com>
2025-03-17 08:59:31 +0530

    Update temp.java

--- temp.java
+++ temp.java
@@ -25,6 +25,12 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
 
 
 //temp
